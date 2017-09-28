e9c9968870af52de1347f1701aeff8fc5046abe0
Danpar97 <Danpar97@github.com>
2017-09-29 03:52:58 +0530

    Added second trial file

--- source/first.cpp
+++ source/first.cpp
@@ -1,44 +1,27 @@
 #include <SDL2/SDL.h>
+#include <iostream>
 
-//compile from terminal: g++ ./main.cpp $(pkg-config --cflags --libs sdl2)
-/*
-SDL_Window *vw = 0;
-SDL_Renderer *pr = 0;
+//compile from terminal: g++ ./first.cpp $(pkg-config --cflags --libs sdl2)
 
-bool init_win(char *tit, int x, int y, int h, int w, int flag){
-    if((SDL_INIT_EVERYTHING) < 0){//failed to initialize
-        return(false);
-    }
-    vw = SDL_CreateWindow(tit, x, y, w, h, flag);
-    if(vw != 0){
-        pr = SDL_CreateRenderer(vw, -1, 0);
-    }
-    return(true);
-}
-
-void render_anim(){
-    //color palette is RGBA
-    SDL_SetRenderDrawColor(pr, 128, 128, 0, 255);
-    SDL_RenderClear(pr);
-    SDL_RenderPresent(pr);
-}
-*/
 int main(){
     if((SDL_INIT_EVERYTHING) < 0){//failed to initialize
+	std::cout << "SDL Failed to initialise.\nSDL Error code: " << SDL_GetError() << std::endl;
         return(-1);
     }
-    SDL_Window *vw = 0;
-    SDL_Renderer *pr = 0;
-    vw = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *w = 0;
+    SDL_Renderer *r = 0;
+    w = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
-    if(vw != 0){
-        pr = SDL_CreateRenderer(vw, -1, 0);
+    if(w == 0){
+	std::cout << "SDL failed to create a new window.\nSDL Error code: " << SDL_GetError() << std::endl;
+	return(-2);
     }
+    r = SDL_CreateRenderer(vw, -1, 0);
 
     //color palette is RGBA sequentially
     SDL_SetRenderDrawColor(pr, 128, 128, 0, 255);
-    SDL_RenderClear(pr);
-    SDL_RenderPresent(pr);
+    SDL_RenderClear(r);
+    SDL_RenderPresent(r);
 
     //delay before closing in milliseconds
     SDL_Delay(5000);
--- source/second.cpp
+++ source/second.cpp
@@ -1,25 +1,46 @@
 #include <SDL2/SDL.h>
+#include <iostream>
 
-//compile from terminal: g++ ./main.cpp $(pkg-config --cflags --libs sdl2)
-/*
-SDL_Window *vw = 0;
-SDL_Renderer *pr = 0;
+//compile from terminal: g++ ./second.cpp $(pkg-config --cflags --libs sdl2)
 
-bool init_win(char *tit, int x, int y, int h, int w, int flag){
+SDL_Window *w = 0;
+SDL_Renderer *r = 0;
+
+bool init_win(const char *tit, int x, int y, int ht, int wt, int flag){
     if((SDL_INIT_EVERYTHING) < 0){//failed to initialize
-        return(false);
+        std::cout << "SDL Failed to initialise.\nSDL Error code: " << SDL_GetError() << std::endl;
+	return(false);
     }
-    vw = SDL_CreateWindow(tit, x, y, w, h, flag);
-    if(vw != 0){
-        pr = SDL_CreateRenderer(vw, -1, 0);
+    w = SDL_CreateWindow(tit, x, y, wt, ht, flag);
+    if(w == 0){//failed to create a new window
+	std::cout << "SDL failed to create a new window.\nSDL Error code: " << SDL_GetError() << std::endl;
+	return(false);
     }
+    //
+    r = SDL_CreateRenderer(w, -1, 0);
     return(true);
 }
 
-void render_anim(){
+void render_anim(int8_t col = 0){
     //color palette is RGBA
-    SDL_SetRenderDrawColor(pr, 128, 128, 0, 255);
-    SDL_RenderClear(pr);
-    SDL_RenderPresent(pr);
+    SDL_SetRenderDrawColor(r, 0 + col, 0 + (col%2), 0 + (col%3), 255);
+    SDL_RenderClear(r);
+    SDL_RenderPresent(r);
 }
-*/
+
+int main(){
+   if(!(init_win("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 720, SDL_WINDOW_SHOWN))){
+	return(-1);
+   }
+
+   int8_t count = 0;
+   while(count < 256){
+	render_anim(count++);
+        SDL_Delay(500);
+   }
+
+   //Cleanup
+   SDL_DestroyWindow(w);
+   SDL_Quit();
+   return(0);
+}
