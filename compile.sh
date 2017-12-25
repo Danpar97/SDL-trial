3bbb83c9b35104e6b2be5ae95bcf19ffb7e4c9d6
danpar97 <danish.parvez19@gmail.com>
2017-12-25 15:28:46 +0530

    Fixed compile.sh
    modify the file for changing compilation targets

--- compile.sh
+++ compile.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-echo "Enter filename: ";
-read $FILE_NAME;
+#FILE_PATH='/home/danish/Repositories/SDL-trial/source/'
+#$C_TARGETS=$('/home/danish/Repositories/SDL-trial/source/experimental.cpp')
 
 set -e    # halt on first error
 
 # use 'g++' as the compiler
-CPP=g++
+CPP='g++'
 
 # be aggressive about warnings and errors
-FLAGS="-lSDL2 -lSDL2main -lSDL2_image -Wall -Wextra -Werror -Wfatal-errors -Wpedantic -pedantic-errors"
+W_FLAGS='-Wall -Wextra -Werror -Wfatal-errors -Wpedantic -pedantic-errors'
+INC_FLAGS='-I/usr/include/SDL2'
+SDL_FLAGS='-lSDL2 -lSDL2main -lSDL2_image'
 
 # clear out old object files
-rm -f *.o
+rm -f /home/danish/Repositories/SDL-trial/source/*.o
 
 # compile
+$CPP $INC_FLAGS -c /home/danish/Repositories/SDL-trial/source/experimental.cpp -o /home/danish/Repositories/SDL-trial/source/experimental.o $W_FLAGS
+$CPP -o /home/danish/Repositories/SDL-trial/source/experimental /home/danish/Repositories/SDL-trial/source/experimental.o $SDL_FLAGS $W_FLAGS
 
-$CPP $FLAGS -o binary.out $FILE_NAME
-
-# compile from terminal: g++ -o experimental ./experimental.cpp $(pkg-config --cflags --libs SDL2 SDL2main SDL2_image)
-# g++  -I/usr/include/SDL2 -c /home/danish/Repositories/SDL-trial/source/experimental.cpp -o /home/danish/Repositories/SDL-trial/source/experimental.o
-# g++  -o /home/danish/Repositories/SDL-trial/source/experimental /home/danish/Repositories/SDL-trial/source/experimental.o  -lSDL2 -lSDL2main -lSDL2_image
+# (outdated method) compile from terminal: g++ -o experimental ./experimental.cpp $(pkg-config --cflags --libs SDL2 SDL2main SDL2_image)
+# Two step compilation
+#g++  -I/usr/include/SDL2 -c /home/danish/Repositories/SDL-trial/source/experimental.cpp -o /home/danish/Repositories/SDL-trial/source/experimental.o
+#g++  -o /home/danish/Repositories/SDL-trial/source/experimental /home/danish/Repositories/SDL-trial/source/experimental.o  -lSDL2 -lSDL2main -lSDL2_image
